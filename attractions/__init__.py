cffc4c3abca9a0cefbd7311cb367e6d92190af6a
Devin Garcia <devinmgarcia@gmail.com>
2021-07-14 10:22:24 -0500

    finished seven

---
+++ attractions/__init__.py
@@ -0,0 +1,1 @@
+from .attractions import SnakePit, PettingZoo, Wetlands
---
+++ attractions/attractions.py
@@ -0,0 +1,27 @@
+class SnakePit:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        self.animals = list()
+
+    def add_animal(self, animal):
+        self.animals.append(animal)
+
+class PettingZoo:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        self.animals = list()
+
+    def add_animal(self, animal):
+        self.animals.append(animal)
+
+class Wetlands:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        self.animals = list()
+
+    def add_animal(self, animal):
+        self.animals.append(animal)
+
